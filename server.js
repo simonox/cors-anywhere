23af0edd1f4fd1313f5b6a1c5945c7896cf717a0
Oliver Ochs <oliver.ochs@holisticon.de>
2017-11-20 18:01:14 +0100

    Added headers

--- server.js
+++ server.js
@@ -39,6 +39,9 @@ cors_proxy.createServer({
     // Do not add X-Forwarded-For, etc. headers, because Heroku already adds it.
     xfwd: false,
   },
+  setHeaders: {
+    Referer: "https://hsp-hh.sport.uni-hamburg.de/angebote/aktueller_zeitraum/_Badminton.html"
+  },
 }).listen(port, host, function() {
   console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
